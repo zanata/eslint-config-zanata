a162fe2f8135d0a99b71945edb64d3abba978019
Sean Flanigan <sean@flanigan.org>
2018-06-27 17:33:01 +1000

    Add link for standard-react

--- react.js
+++ react.js
@@ -1,5 +1,6 @@
 // This config just adds support for React and es6 parsing
 
 module.exports = {
+  // standard-react: see https://github.com/standard/eslint-config-standard-react
   extends: ['zanata', 'standard-react']
 }
